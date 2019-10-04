fb1ac6490289e7eacb00d6646a379c77b61e3d77
Jordan Smith <great.jordini@gmail.com>
2019-10-04 16:14:52 -0600

    Initial Commit

---
+++ Damage Report Converter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Damage Report Converter
+//
+//  Created by Jordan Smith on 10/4/19.
+//  Copyright © 2019 Jordan Smith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Damage Report Converter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Damage Report Converter
+//
+//  Created by Jordan Smith on 10/4/19.
+//  Copyright © 2019 Jordan Smith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
